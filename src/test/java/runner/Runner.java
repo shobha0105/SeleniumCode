0d8b577c65119bce64cd0f21ae503c602da358b4
shobha0105 <44916515+shobha0105@users.noreply.github.com>
2018-11-14 09:31:36 +0530

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features="src\\test\\java\\features" , 
 		glue= {"stepdef"},
-		tags= {"@FA"}, //~ for NOT //without ~ for running specific feature file
+		tags= {}, //~ for NOT //without ~ for running specific feature file
 		monochrome=true, //for the output to be readable
 		dryRun=false
 		
